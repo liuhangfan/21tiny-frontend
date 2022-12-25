2bc8aa878b6d01868479786da247dedfdd487e33
Hangfan Liu <liuhangfan27@gmail.com>
2022-12-25 15:04:45 -0800

    add input checker

--- src/components/UrlInput.js
+++ src/components/UrlInput.js
@@ -4,50 +4,91 @@ import Box from '@mui/material/Box';
 import { Input } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import LoadingButton from '@mui/lab/LoadingButton';
+import validator from 'validator'
+import TextField from '@mui/material/TextField';
+
 const UrlInput = ({setInputValue, loading, setLoading}) => {
     const [value, setValue] = useState({
         "url": "",
         "alias": ""
-      });
+    });
+    const [urlOK,setUrlOK] = useState(true)
+    const [aliasOK,setAliasOK] = useState(true)
+
+    const inputTips = {
+        "URLsuccess": "Paste a link to tiny it",
+        "URLfailed": "Incorrect URL",
+        "AliasSuccess": "type your alias (optional)",
+        "AliasFailed":"Incorrect alias",
+    };
+
+
     // const [loading, setLoading] = useState(false);
 
     function handleClick(){
-        //TODO check input
-        setLoading(true);
-        setInputValue(value);
-        // setValue({
-        //     "url": "",
-        //     "alias": ""
-        // });
+        var ok = true;
+        if (!validator.isURL(value.url)){
+            setUrlOK(false);
+            ok = false;
+        }
+        if (value.alias && value.alias.length >= 12){
+            setAliasOK(false);
+            ok = false;
+        }
+        if (ok){
+            setLoading(true);
+            setInputValue(value);
+        }
     }
+
+    
 
     return(
         <div className="relative">
             <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
                 <Box gridColumn="span 12">
-                    <Input
+                    <TextField
+                    required
+                    label="URL"
+                    variant="standard"
                         type="text"
-                        placeholder="Paste a link to tiny it"
+                        placeholder={inputTips.URLsuccess}
+                        helperText= {urlOK? "" : inputTips.URLfailed}
+                        error = {!urlOK}
                         fullWidth={true}
                         value={value.url}
-                        onChange={e => setValue({ ...value, url: e.target.value })}
+                        onChange={
+                            e => {setValue({ ...value, url: e.target.value })
+                            setUrlOK(true);}
+                        }
                         />
                 </Box>
                 <Box gridColumn="span 4">
-                    <Input
+                    <TextField
+                    variant="standard"
                         type="text"
-                        placeholder="www.url.21tiny.com/"
-                        readOnly = {true}
+                        label="Domain/"
+                        placeholder="url.21tiny.com/"
+                        InputProps={{
+                            readOnly: true,
+                          }}
                         />
                 </Box>
                 <Box gridColumn="span 6">
-                    <Input
-                            type="text"
-                            placeholder="alias (optional)"
-                            value={value.alias}
-                            fullWidth={true}
-                            onChange={e => setValue({ ...value, alias: e.target.value })}
-                            />
+                <TextField
+                    variant="standard"
+                    label="alias"
+                        type="text"
+                        placeholder={inputTips.AliasSuccess}
+                        helperText= {aliasOK? "" : inputTips.AliasFailed}
+                        error = {!aliasOK}
+                        fullWidth={true}
+                        value={value.alias}
+                        onChange={
+                            e => {setValue({ ...value, alias: e.target.value })
+                            setAliasOK(true);}
+                        }
+                        />
                 </Box>
                 <Box gridColumn="span 2">
                     <LoadingButton
