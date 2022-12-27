04926f301f950ea94258f4e4f37db9e282bfd92f
Hangfan Liu <liuhangfan27@gmail.com>
2022-12-27 00:25:33 -0800

    fix solutions part

--- src/components/works.js
+++ src/components/works.js
@@ -11,13 +11,10 @@ export default function Works() {
                     How It Works
                 </h1>
                 <div className="container mx-auto p-2 flex md:flex-row flex-col">
-            {/* <div className="lg:w-1/3 md:w-1/3 bg-[#208ef4] rounded-lg overflow-hidden sm:mr-5 p-5 items-end  relative">
-                <p>placeholder</p>
-            </div> */}
             <img src={storageIMG} className="img bg-gradient-to-l hover:bg-gradient-to-r" alt="img" />
                 <div className="lg:w-2/3 md:w-2/3 rounded-lg overflow-hidden sm:mr-5 p-5 items-end  relative">
-                <h2 className="text-lg font-medium title-font mb-4 text-white">Our product is a tool that transforms your long links into shorter ones. It can generate drastically shorten your long links and still reliably direct to the same web pages. To use our link shortener, the user only has to copy and paste their long link into our tool and we condense it into a more shareable link. Our links are reliable, safe, and never expire.</h2>
-                <h2 className="text-lg font-medium title-font mb-4 text-white">This project is completely open source , the back-end system is built by Springboot , the storage layer relies on mongoDB. short ID generation algorithm is a combination of snowflake algorithm and BASE64 . The front-end system is built by react, JavaScript. If you are interested in this project, please do not hesitate to check out our github.</h2>
+                <h2 className="text-lg font-medium title-font mb-4 text-white">Our product is a tool that transforms your long URL into shorter ones. It can drastically tiny your long links and direct to the same web pages. Our links are reliable, safe, and never expire.</h2>
+                <h2 className="text-lg font-medium title-font mb-4 text-white">This project is completely open source , the back-end system is built by Spring Boot, the storage layer relies on mongoDB, the tiny ID generation algorithm is a combination of snowflake algorithm and BASE64 encoder. And the front-end system is built by react, JavaScript. If you are interested in this project, please do not hesitate to check out our open source code at github.</h2>
                 </div>
             </div>
             </div>
